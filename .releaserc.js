385ffd1cee8f08aa64ff6dc09e5787760f284b5e
Brian Torres-Gil <brian@ixi.us>
2020-08-25 14:10:52 -0700

    ci: Move to semantic-release action

--- .releaserc.js
+++ .releaserc.js
@@ -9,19 +9,7 @@ module.exports = {
         npmPublish: false,
       },
     ],
-    [
-      '@semantic-release/git',
-      {
-        message:
-          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
-      },
-    ],
-    [
-      '@semantic-release/github',
-      {
-        successComment:
-          ":tada: This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${nextRelease.version} :tada:\n\nMore details on this release are available in the [GitHub release](<github_release_url>).\n\n> Posted by [semantic-release](https://github.com/semantic-release/semantic-release) bot",
-      },
-    ],
+    '@semantic-release/git',
+    '@semantic-release/github',
   ],
 }
